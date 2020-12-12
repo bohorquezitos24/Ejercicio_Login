943c3f1edcca39328f4e4e81e10271b106b4881a
bohorquezitos24 <nbohorquez24@misena.edu.co>
2020-12-11 21:03:59 -0500

    actualizacion

--- login/login.js
+++ login/login.js
@@ -1,9 +1,38 @@
+function validar_email( email ) 
+{
+    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email) ? true : false;
+}
 
 
-if (navigator.appName=="Google Chrome"){
-    location.href="inicio.js";
- }
- if (navigator.appName=="Google Chrome"){
+
+var email = "bohorquezitos@hotmail.com";
  
-    location.href="http://https://www.youtube.com/watch?v=pnGiaGcBJQM";
- }
+if( validar_email(email ) )
+{
+    alert("El email es correcto");
+}
+else
+{
+    alert("El email NO es correcto");
+}
+function validarPasswd () {
+   
+   var passwd = document.getElementById("passwd").value;
+
+
+ 
+
+    }
+    function validador () {
+       console.log("validador")
+       var email = document.getElementById("email").value;
+       var passwd = document.getElementById("passwd").value;
+   if (email =='bohorquezitos@hotmail.com' & passwd == '1234') 
+   {
+      window.open("./inicio.html")
+   }
+   
+   else {document.getElementById ("validador").innerHTML = 'Datos correctos' 
+   }
+}
